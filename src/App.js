620e35f687c287aed44fa00724251fd88fc03de1
Rancha124 <charansaipasupula98@gmail.com>
2024-04-11 12:42:56 -0500

    feat/ weather application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { WeatherInfo } from "./components/weatherInfo";
 
 function App() {
+  const [data, setData] = useState({});
+  const [cityName, setCityName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const options = {
+    method: "GET",
+    url: `https://open-weather13.p.rapidapi.com/city/${cityName}/EN`,
+    headers: {
+      "X-RapidAPI-Key": process.env.REACT_APP_OPENWEATHER_API_KEY,
+      "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+    },
+  };
+
+  const searchCityName = async (event) => {
+    console.log("ohh yes", event.key);
+    if (!cityName) {
+      setError("Please enter city name!");
+      return;
+    }
+    if (event.key === "Enter") {
+      // Set loading to true when starting the fetch
+      setLoading(true);
+      try {
+        const response = await axios.request(options);
+        setData(response.data);
+        if (response.data.cod === "404") {
+          setError(
+            `Please make sure the city name ${cityName} you entered is correct`
+          );
+          setData({});
+        } else {
+          setError("");
+          setData(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      } finally {
+        // Clear the input field and Set loading to false when fetch is complete
+        setLoading(false);
+        setCityName("");
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="search">
+        <input
+          value={cityName}
+          onChange={(event) => {
+            setCityName(event.target.value);
+            setError("");
+          }}
+          onKeyPress={searchCityName}
+          placeholder="Enter City Name"
+          type="text"
+        />
+
+        <button
+          className="icon"
+          onClick={() => searchCityName({ key: "Enter" })}
         >
-          Learn React
-        </a>
-      </header>
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
+      <h2 className="title">Welcome to the Weather App!</h2>
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : error ? (
+        <div className="error">{error}</div>
+      ) : (
+        <div className="container">
+          <WeatherInfo data={data} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/weatherInfo.js
@@ -0,0 +1,41 @@
+import "../App.css";
+export const WeatherInfo = ({ data }) => {
+  return (
+    <>
+      <div className="main-weather-info">
+        <div className="cityname">
+          <p>{data.name}</p>
+        </div>
+        <div className="temp">
+          {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+        </div>
+        <div className="description">
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+        </div>
+      </div>
+
+      {data.name !== undefined && data.main && (
+        <div className="sub-weather-info">
+          {data.main.feels_like ? (
+            <div className="feels">
+              <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              <p>Feels Like</p>{" "}
+            </div>
+          ) : null}
+          {data.main.humidity ? (
+            <div className="humidity">
+              <p className="bold">{data.main.humidity}%</p>
+              <p>Humidity</p>
+            </div>
+          ) : null}
+          {data.wind.speed ? (
+            <div className="wind">
+              <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              <p>Wind Speed</p>
+            </div>
+          ) : null}
+        </div>
+      )}
+    </>
+  );
+};
