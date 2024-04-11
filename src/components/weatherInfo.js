5962528d72fc736782649846cfe153cd370d0654
Rancha124 <charansaipasupula98@gmail.com>
2024-04-11 17:02:35 -0500

    feat: pressure and visibility added

--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -16,24 +16,40 @@ export const WeatherInfo = ({ data }) => {
 
       {data.name !== undefined && data.main && (
         <div className="sub-weather-info">
-          {data.main.feels_like ? (
-            <div className="feels">
-              <p className="bold">{data.main.feels_like.toFixed()}°F</p>
-              <p>Feels Like</p>{" "}
-            </div>
-          ) : null}
-          {data.main.humidity ? (
-            <div className="humidity">
-              <p className="bold">{data.main.humidity}%</p>
-              <p>Humidity</p>
-            </div>
-          ) : null}
-          {data.wind.speed ? (
-            <div className="wind">
-              <p className="bold">{data.wind.speed.toFixed()} MPH</p>
-              <p>Wind Speed</p>
-            </div>
-          ) : null}
+          <div className="row">
+            {data.main.feels_like ? (
+              <div className="feels">
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+                <p>Feels Like</p>{" "}
+              </div>
+            ) : null}
+            {data.main.humidity ? (
+              <div className="humidity">
+                <p className="bold">{data.main.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            ) : null}
+            {data.wind.speed ? (
+              <div className="wind">
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                <p>Wind Speed</p>
+              </div>
+            ) : null}
+          </div>
+          <div className="row">
+            {data.main.pressure ? (
+              <div className="wind">
+                <p className="bold">{data.main.pressure.toFixed()} hPa</p>
+                <p>Pressure</p>
+              </div>
+            ) : null}
+            {data.visibility ? (
+              <div className="wind">
+                <p className="bold">{data.visibility.toFixed()} m</p>
+                <p>Visibility</p>
+              </div>
+            ) : null}
+          </div>
         </div>
       )}
     </>
